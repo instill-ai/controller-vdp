c48792a82b4f28e4ece36e2ae2500c9ed0d57411
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-07-05 16:56:20 +0800

    chore: update integration test params (#2)
    Because
    
    - Some of our test-cases may be cross projects, we need to use different
    apigateway endpoints
    
    This commit
    
    - update integration test params

--- integration-test/const.js
+++ integration-test/const.js
@@ -1,14 +1,14 @@
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 let proto
-let pHost, cHost, mgHost, ctHost
-let pPublicPort, pPrivatePort, cPublicPort, cPrivatePort, mgPublicPort, mgPrivatePort, ctPrivatePort
+let pHost, cHost, ctHost
+let pPublicPort, pPrivatePort, cPublicPort, cPrivatePort, mgPrivatePort, ctPrivatePort
 
-if (__ENV.API_GATEWAY_HOST && !__ENV.API_GATEWAY_PORT || !__ENV.API_GATEWAY_HOST && __ENV.API_GATEWAY_PORT) {
-  fail("both API_GATEWAY_HOST and API_GATEWAY_PORT should be properly configured.")
+if (__ENV.API_GATEWAY_VDP_HOST && !__ENV.API_GATEWAY_VDP_PORT || !__ENV.API_GATEWAY_VDP_HOST && __ENV.API_GATEWAY_VDP_PORT) {
+  fail("both API_GATEWAY_VDP_HOST and API_GATEWAY_VDP_PORT should be properly configured.")
 }
 
-export const apiGatewayMode = (__ENV.API_GATEWAY_HOST && __ENV.API_GATEWAY_PORT);
+export const apiGatewayMode = (__ENV.API_GATEWAY_VDP_HOST && __ENV.API_GATEWAY_VDP_PORT);
 
 if (__ENV.API_GATEWAY_PROTOCOL) {
   if (__ENV.API_GATEWAY_PROTOCOL !== "http" && __ENV.API_GATEWAY_PROTOCOL != "https") {
@@ -21,15 +21,13 @@ if (__ENV.API_GATEWAY_PROTOCOL) {
 
 if (apiGatewayMode) {
   // api-gateway mode
-  pHost = cHost = mgHost = ctHost = __ENV.API_GATEWAY_HOST
+  pHost = cHost = ctHost = __ENV.API_GATEWAY_VDP_HOST
   pPrivatePort = 3081
   cPrivatePort = 3082
-  mgPrivatePort = 3084
   ctPrivatePort = 3085
-  pPublicPort = cPublicPort = mgPublicPort = 8080
+  pPublicPort = cPublicPort = 8080
 } else {
   // direct microservice mode  
-  mgHost = "mgmt-backend"
   pHost = "pipeline-backend"
   cHost = "connector-backend"
   ctHost = "controller-vdp"
@@ -39,15 +37,12 @@ if (apiGatewayMode) {
   ctPrivatePort = 3085
   pPublicPort = 8081
   cPublicPort = 8082
-  mgPublicPort = 8084
 }
 
 export const connectorPublicHost = `${proto}://${cHost}:${cPublicPort}`;
 export const connectorPrivateHost = `${proto}://${cHost}:${cPrivatePort}`;
 export const pipelinePublicHost = `${proto}://${pHost}:${pPublicPort}`;
 export const pipelinePrivateHost = `${proto}://${pHost}:${pPrivatePort}`;
-export const mgmtPublicHost = `${proto}://${mgHost}:${mgPublicPort}`;
-export const mgmtPrivateHost = `${proto}://${mgHost}:${mgPrivatePort}`;
 export const controllerPrivateHost = `${proto}://${ctHost}:${ctPrivatePort}`;
 
 export const controllerGRPCPrivateHost = `${ctHost}:${ctPrivatePort}`;
